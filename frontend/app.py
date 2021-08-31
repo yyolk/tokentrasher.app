17117197ad8669af9130eb2e627a87eb5c59494e
Joseph Chiocchi <joe@yolk.cc>
2021-08-31 18:04:57 -0500

    update frontend app and client w/ basic exc handling

--- frontend/app.py
+++ frontend/app.py
@@ -7,14 +7,16 @@ xumm_client = XUMM()
 
 def handler(event, context):
 
+    custom_identifier = "test"
     payload_resp_json = xumm_client.post_payload(
         txjson={"TransactionType": "SignIn"},
         options={"return_url": {
             "app": "https://tokentrash.app",
             "web": "https://tokentrash.app",
         }},
-        custom_meta={"identifier": "test"},
+        custom_meta={"identifier": custom_identifier},
     )
+    print(payload_resp_json)
     qr_png = payload_resp_json["refs"]["qr_png"]
 
 
--- frontend/client.py
+++ frontend/client.py
@@ -1,8 +1,26 @@
 import os
 
-from uplink import Consumer, Field, get, headers, json, post, returns
+from uplink import Consumer, Field, get, headers, json, post, response_handler, returns
 
 
+class FrontendError(Exception):
+    """Customer Error for our app"""
+
+
+# def raise_api_error(exc_type, exc_val, exc_tb):
+#     raise FrontendError(exc_val)
+#
+
+
+def raise_if_api_error(response):
+    if response.status_code == 200:
+        return response
+    if "error" in (response_json := response.json()):
+        raise FrontendError(response_json["error"])
+    raise FrontendError("Unknown Error occurred %s", response)
+
+
+@response_handler(raise_if_api_error)
 @headers(
     {
         "X-API-Key": os.environ["XUMM_API_KEY"],
@@ -27,7 +45,6 @@ class XUMM(Consumer):
     #     self, txjson: Field, options: Field = None, custom_meta: Field = None
     # ):
     #     """https://xumm.readme.io/reference/post-payload"""
-
 
     @get("payload/{payload_uuid}")
     def get_payload(self, payload_uuid):
